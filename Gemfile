07f1c270c12184244ba1c7f6c49f3feaa38b2c83
Yukke Kamisuna <yukke5222@gmail.com>
2018-08-04 08:31:33 +0900

    Finish static pages

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ group :development, :test do
   gem 'sqlite3', '1.3.13'
   gem 'byebug',  '9.0.6', platform: :mri
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'rails-controller-testing', '1.0.2'
 end
 
 group :development do
@@ -23,7 +25,6 @@ group :development do
 end
 
 group :test do
-  gem 'rails-controller-testing', '1.0.2'
   gem 'minitest-reporters',       '1.1.14'
   gem 'guard',                    '2.13.0'
   gem 'guard-minitest',           '2.4.4'
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+      assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
     end
   end
 
@@ -13,6 +15,23 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :help
       expect(response).to have_http_status(:success)
+      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+      assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+      assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
     end
   end
 
