a304bd6627fab3da353f441d3c9d8019cea77fd4
Yukke Kamisuna <yukke5222@gmail.com>
2018-08-04 15:47:39 +0900

    Finish layout and routes

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
-      assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+      assert_select "title", "Ruby on Rails Tutorial Sample App"
     end
   end
 
